4159661f506a6b3b34eef1a636f98af6c9fcb746
Kenneth Tay <kenneth21tay@gmail.com>
2025-09-23 17:56:29 +0800

    Setting up git.

---
+++ src/main/java/com/chat_project/Chatty/ChattyApplication.java
@@ -0,0 +1,13 @@
+package com.chat_project.Chatty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat_project/Chatty/ChattyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat_project.Chatty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
