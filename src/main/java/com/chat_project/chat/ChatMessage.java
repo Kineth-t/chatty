f8ce8d9d8b4fe570cfc950b7df25541c97e97c78
Kenneth Tay <kenneth21tay@gmail.com>
2025-09-28 17:54:57 +0800

    Setting up ChatMessage class.

---
+++ src/main/java/com/chat_project/chat/ChatMessage.java
@@ -0,0 +1,28 @@
+package com.chat_project.chat;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document
+public class ChatMessage {
+    @Id
+    private String id;
+    private String chatId;
+    private String sender;
+    private String recipient;
+    private String content;
+    private Date timestamp;
+}
