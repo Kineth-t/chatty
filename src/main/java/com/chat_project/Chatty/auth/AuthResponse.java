d6e88aaad727650e2e4c2040cc02495d99da19b5
Kenneth Tay <kenneth21tay@gmail.com>
2025-10-10 15:37:45 +0800

    Created AuthResponse.java DTO.

--- src/main/java/com/chat_project/Chatty/auth/AuthResponse.java
+++ src/main/java/com/chat_project/Chatty/auth/AuthResponse.java
@@ -1,5 +1,16 @@
 package com.chat_project.Chatty.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class AuthResponse {
-    
+    private String token;
+    private String username;
+    private String email;
 }
