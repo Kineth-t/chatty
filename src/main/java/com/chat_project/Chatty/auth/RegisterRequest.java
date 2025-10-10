a68d5910e0aa25fe5aaafcb1de4f6b6c8ad8d5ce
Kenneth Tay <kenneth21tay@gmail.com>
2025-10-10 15:32:16 +0800

    Created RegisterRequest.java DTO.

--- src/main/java/com/chat_project/Chatty/auth/RegisterRequest.java
+++ src/main/java/com/chat_project/Chatty/auth/RegisterRequest.java
@@ -1,5 +1,16 @@
 package com.chat_project.Chatty.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RegisterRequest {
-    
+    private String username;
+    private String email;
+    private String password;
 }
