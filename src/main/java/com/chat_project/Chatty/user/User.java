7dd1a94b7bf9f547090180ebbf2030a9e9557a35
Kenneth Tay <kenneth21tay@gmail.com>
2025-10-10 15:37:02 +0800

    Created LoginRequest.java DTO.

--- src/main/java/com/chat_project/Chatty/user/User.java
+++ src/main/java/com/chat_project/Chatty/user/User.java
@@ -1,6 +1,7 @@
 package com.chat_project.Chatty.user;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Getter;
@@ -11,7 +12,10 @@ import lombok.Setter;
 @Document
 public class User {
     @Id
+    private String id;
+    @Indexed(unique = true)
     private String username;
+    @Indexed(unique = true)
     private String email;
     private String password;
     private Status status;
