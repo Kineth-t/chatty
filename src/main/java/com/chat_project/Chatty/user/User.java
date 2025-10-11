8d553194314345498ab52c0ee0e94f44403c5b81
Kenneth Tay <kenneth21tay@gmail.com>
2025-10-11 13:03:23 +0800

    Implemented JwtService.java

--- src/main/java/com/chat_project/Chatty/user/User.java
+++ src/main/java/com/chat_project/Chatty/user/User.java
@@ -3,22 +3,70 @@ package com.chat_project.Chatty.user;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Document
-public class User {
+public class User implements UserDetails {
     @Id
     private String id;
+    
     @Indexed(unique = true)
     private String username;
+    
     @Indexed(unique = true)
     private String email;
+    
     private String password;
     private Status status;
+    
+    // UserDetails implementation
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
