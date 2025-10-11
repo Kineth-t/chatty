9c117b160747d916909c85161a4a26122e4dc55a
Kenneth Tay <kenneth21tay@gmail.com>
2025-10-11 11:57:23 +0800

    Remove token in response body, instead send a message, since we are saving token in cookies instead.

--- src/main/java/com/chat_project/Chatty/auth/AuthResponse.java
+++ src/main/java/com/chat_project/Chatty/auth/AuthResponse.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AuthResponse {
-    private String token;
     private String username;
     private String email;
+    private String message;
 }
